742830edc453388870a7990f714e4dca2f9d9cb7
NelsonCifuentes <67082174+NelsonCifuentes@users.noreply.github.com>
2021-08-28 22:43:00 -0500

    Initial commit

---
+++ resources/views/users/todos-los-registro.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NOMBRES</th>
+                                <th>APELLIDOS</th>
+                                <th>CORREOA</th>
+                                <th>ACCIONES</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->id}}</td>
+                                <td>{{ $user->name}}</td>
+                                <td>{{ $user->lastnames}}</td>
+                                <td>{{ $user->email}}</td>
+                                <td>
+                                    <a href="">Detalles</a>
+                                    <a href="">Editar</a>
+                                    <a href="">Eliminar</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
